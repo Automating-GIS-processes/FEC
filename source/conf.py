d3b2c8c4a83b1cecc2a993888056fd942ec2b97c
VuokkoH <vuokko.heikinheimo@helsinki.fi>
2017-02-20 17:25:01 +0200

    updating conf.py

--- source/conf.py
+++ source/conf.py
@@ -39,7 +39,7 @@ extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
-    #'sphinx.ext.githubpages',
+    'sphinx.ext.githubpages',
 
     # IPython console
     'IPython.sphinxext.ipython_console_highlighting',
